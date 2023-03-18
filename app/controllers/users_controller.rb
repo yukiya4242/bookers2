55b0bae14e94d653be4cb73a0e73608104fa57e4
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-03-18 11:16:49 +0000

    [finish]2-27課題３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def edit
    @user = User.find(params[:id])
    if @user != current_user
-    redirect_to edit_user_path
+    redirect_to user_path(current_user)
    end
 
   end
